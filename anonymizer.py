a8b07955e0e28d602b6ea06eb8ddebe804e30362
Aiden-Koorsen <aidenkoorsen@outlook.com>
2023-11-23 15:48:33 +0200

    Finished the project, just need to optimzie

--- anonymizer.py
+++ anonymizer.py
@@ -8,21 +8,28 @@ import os
 from blur import blur_video, blur_image
 
 # Make sure of proper usage
-if len(sys.argv) != 2:
-	print("Error -- wrong amount of arguments\n Usage: python anonymizer.py IN_FILE")
+if len(sys.argv) != 4:
+	print("Error -- wrong amount of arguments\n Usage: python anonymizer.py IN_FILE FACES PLATES")
 	quit()
 
 # Get file and determine type and handle any errors
 filename = sys.argv[1]
 extension = filename.split(".")[1]
+want_faces = sys.argv[2]
+want_plates = sys.argv[3]
+
+# Make sure fomat of flags are correct
+if (want_faces != ("true" or "false")) and (want_plates != ("true" or "false")):
+	print("Error -- use true an false for FACES and PLATES flags")
+	exit()	
 
 if os.path.exists(filename) == False:
 	print("Error -- file " + filename + " does not exist")
 	exit()
 
 if extension == "mp4":
-	blur_video(filename)
+	blur_video(filename, want_faces, want_plates)
 elif extension == "png" or extension == "jpeg" or extension == "jpg":
-	blur_image(filename)
+	blur_image(filename, want_faces, want_plates)
 else:
 	print("Error -- Unrecognized file format please use:\n PNG, JPEG, JPG for images\nMP4 for video")
--- blur.py
+++ blur.py
@@ -3,6 +3,10 @@
 import cv2 as cv
 import numpy as np
 from PIL import Image
+import moviepy.editor as mpe
+import time
+import threading
+import os
 
 def blur(image, area):
 	cropped = image.crop((area[0], area[1], area[0] + area[2], area[1] + area[3]))
@@ -29,8 +33,32 @@ if not plate_cascade.load("data/haarcascade_russian_plate_number.xml"):
 	print("Error -- Loading number-plate cascade")
 	exit()
 
+processed_faces = []
+processed_plates = []
 
-def blur_video(filename):
+
+# simple functions for processing a chunk of images and returning the faces and numberplates
+def process_face_chunk(frames):
+	faces = []
+	for frame in frames:
+		faces = face_cascade.detectMultiScale(frame)
+		processed_faces.append(faces)
+		
+def process_plate_chunk(frames):
+	plates = []
+	for frame in frames:
+		plates = plate_cascade.detectMultiScale(frame)
+		processed_plates.append(plates)
+#
+
+
+def blur_video(filename, want_faces, want_plates):
+	start_time = time.time()
+
+	# Filenames set up
+	temp_filename = filename.split('.')[0] + "_temp.mp4"
+	final_filename = filename.split('.')[0] + "_blurred.mp4"
+
 	# Load video
 	vid = cv.VideoCapture(filename)
 	
@@ -40,51 +68,104 @@ def blur_video(filename):
 
 	vid_width = vid.get(cv.CAP_PROP_FRAME_WIDTH)
 	vid_height = vid.get(cv.CAP_PROP_FRAME_HEIGHT)
-	
-	# Define codec and writer to change video
-	fourcc = cv.VideoWriter_fourcc("m", "p", "4", "v")
-	out = cv.VideoWriter(filename.split('.')[0] + "_new.mp4", fourcc, 30.0, (int(vid_width), int(vid_height)), True)
+	frame_count = int(vid.get(cv.CAP_PROP_FRAME_COUNT))
+	fps = vid.get(cv.CAP_PROP_FPS)
 
-	# Loop until video is finished
-	while True:
+	video_frames = []
+	frames = []
+
+	# Get and store all frames to process
+	for i in range(frame_count):
 		ret, frame = vid.read()
-		
-		if not ret:
-			print("No more frames: Exiting")
-			break
 
 		gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-		
-		# Get faces and plates from frame
-		faces = face_cascade.detectMultiScale(gray)
-		number_plates = plate_cascade.detectMultiScale(gray)
-		
-		actual_img = Image.fromarray(frame).convert('RGB')
+		frames.append(gray)
+		video_frames.append(Image.fromarray(frame).convert('RGB'))
 
-		# Blur
-		for face in faces:
-			blur(actual_img, face)
+	# Split data into two chuncks
+	HALF_LENGTH = int(len(frames) / 2)
+	chunk1 = frames[:HALF_LENGTH]
+	chunk2 = frames[HALF_LENGTH:len(frames) - HALF_LENGTH]
 
-		for plate in number_plates:
-			blur(actual_img, plate)			
+	# Process data if the user wants to
+	if want_faces:
+		face_thread1 = threading.Thread(target=process_face_chunk, args=(chunk1,))
 	
-		new_frame = np.array(actual_img)
+	if want_plates:
+		plate_thread1 = threading.Thread(target=process_plate_chunk, args=(chunk1,))
 
-		# Write out to new file
-		out.write(new_frame)
-		
-	# Release the video data	
+	if want_faces:
+		face_thread1.start()
+
+	if want_plates:
+		plate_thread1.start()
+
+	if want_faces:
+		face_thread1.join()
+
+	if want_plates:
+		plate_thread1.join()
+
+	face_thread2 = threading.Thread(target=process_face_chunk, args=(chunk2,))
+	plate_thread2 = threading.Thread(target=process_plate_chunk, args=(chunk2,))
+
+	face_thread2.start()
+	plate_thread2.start()
+	face_thread2.join()
+	plate_thread2.join()
+	print("Processing of images took %s"%(time.time() - start_time))
+	start_time = time.time()
+
+	# Blur all needed areas in each video frane
+	for i in range(len(processed_faces)):
+		for face in processed_faces[i]:
+			blur(video_frames[i], face)
+
+	for i in range(len(processed_plates)):
+		for plate in processed_plates[i]:
+			blur(video_frames[i], plate)
+
+	print("Blurring of video frames took %s"%(time.time() - start_time))
+
+	fourcc = cv.VideoWriter_fourcc("m", "p", "4", "v")
+	out = cv.VideoWriter(temp_filename, fourcc, fps, (int(vid_width), int(vid_height)), True)
+	
+	# Write out video frames
+	for frame in video_frames:
+		out.write(np.array(frame))
+
 	vid.release()
 	out.release()
-		
 
-def blur_image(filename):
+	# Copy audio tracks across files
+	original_vid = mpe.VideoFileClip(filename)
+	original_vid.audio.write_audiofile("data/extracted.mp3")
+
+	new_vid = mpe.VideoFileClip(temp_filename)
+	audio = mpe.AudioFileClip("data/extracted.mp3")
+
+	final = new_vid.set_audio(audio)
+
+	final.write_videofile(final_filename)
+
+	# Remove unessary files
+	os.remove("data/extracted.mp3")
+	os.remove(temp_filename)
+
+
+def blur_image(filename, want_faces, want_plates):
 	# Load the actual image
 	img = cv.imread(filename, 0)
 
 	# Get faces and plates from image
-	faces = face_cascade.detectMultiScale(img)
-	number_plates = plate_cascade.detectMultiScale(img)
+	faces = []
+	number_plates = []
+
+	if want_faces == "true":
+		faces = face_cascade.detectMultiScale(img)
+	
+	if want_plates == "true":
+		number_plates = plate_cascade.detectMultiScale(img)
 	
 	actual_img = Image.open(filename)
 
